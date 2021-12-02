74704cf7951c0028e6dd0a37263d26c73b60e462
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-12-02 20:04:53 +0300

    Create the login user router

--- src/routers/User.js
+++ src/routers/User.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { create } = require("../controllers/User");
+const { create, login } = require("../controllers/User");
 const schemas = require("../validations/User");
 const validate = require("../middlewares/validate");
 
 const router = express.Router();
 
 router.route("/").post(validate(schemas.createValidation), create);
+router.route("/login").post(validate(schemas.loginValidation), login);
 
 module.exports = router;
