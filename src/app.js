e53ea4641b96f9555e73f752cd2f28b0066f9446
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-12-06 19:15:29 +0300

    Lint code

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ const express = require("express");
 const loaders = require("./loaders");
 const config = require("./config");
 const UserRoutes = require("./routers/User");
+const FactoriesListRoutes = require("./routers/FactoriesList");
+
+const cors = require("cors");
 
 loaders();
 config();
 
 const app = express();
 
+app.use(cors({ origin: "*", credentials: true }));
+
 app.use(express.json());
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Listening on ${process.env.APP_PORT}`);
   app.use("/users", UserRoutes);
+  app.use("/factoriesList", FactoriesListRoutes);
 });
