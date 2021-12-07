c03299743b257941d5db99fc4f36b5d93aadf083
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-12-07 13:45:02 +0300

    Add the authenticate and validate to routers

--- src/routers/FactoriesName.js
+++ src/routers/FactoriesName.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const schemas = require("../validations/FactoriesName");
+const validate = require("../middlewares/validate");
+const authenticate = require("../middlewares/authenticate");
 const { create, findAll } = require("../controllers/FactoriesName");
 
 const router = express.Router();
 
-router.route("/").get(findAll);
-router.route("/").post(create);
+router.route("/").get(authenticate, findAll);
+router.route("/").post(validate(schemas.createValidation), create);
 
 module.exports = router;
